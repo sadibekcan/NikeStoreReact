5681b726c1561076c994f81c153f24911eb7df15
Sadi <43606859+BEKCAN@users.noreply.github.com>
2022-10-31 00:53:22 +0300

    nike store react
    nike store react

---
+++ NikeStoreReact/App.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  Image,
+  TouchableOpacity
+} from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
+// SCREENS
+import { Home } from "./screens/";
+
+import { icons, COLORS, FONTS, SIZES } from './constants' 
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent",
+  }
+}
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        initialRouteName={'Home'}
+      >
+        <Stack.Screen 
+          name="Home"
+          component={Home}
+          options={{
+            title: 'SHOE SELECTOR',
+            headerTintColor: COLORS.lightGray,
+            headerTitleStyle: {
+              ...FONTS.navTitle
+            },
+            headerLeft: ({ onPress }) => (
+              <TouchableOpacity
+                style={{ marginLeft: SIZES.padding }}
+                onPress={onPress}
+              >
+                <Image 
+                  source={icons.menu}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25
+                  }}
+                />
+
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity
+                style={{ marginRight: SIZES.padding }}
+                onPress={() => console.log("Header right on pressed")}
+              >
+                <Image 
+                  source={icons.search}
+                  resizeMode="contain"
+                  style={{
+                    width: 30,
+                    height: 30
+                  }}
+                />
+
+              </TouchableOpacity>
+            )
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default () => {
+  return <App />;
+}
